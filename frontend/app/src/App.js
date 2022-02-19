291ef731bdc2584e420af64c26b3b87286dec6e9
kleamp1e <kleamp1e@gmail.com>
2022-02-19 18:28:04 +0900

    ドロップされたファイルのData URLへの変換処理を追加

--- frontend/app/src/App.js
+++ frontend/app/src/App.js
@@ -1,24 +1,37 @@
 import { useCallback } from "react";
-import { useDropzone } from "react-dropzone";
+import Dropzone from "react-dropzone";
 
 import "./App.css";
 
+function loadFile(file) {
+  return new Promise((resolve, reject) => {
+    const fileReader = new FileReader();
+    fileReader.addEventListener("load", () => {
+      resolve(fileReader.result);
+    }, false);
+    fileReader.readAsDataURL(file);
+  });
+}
+
 function MyDropzone() {
-  const onDrop = useCallback((acceptedFiles) => {
-    // Do something with the files
-    console.log({acceptedFiles});
-  }, [])
-  const {getRootProps, getInputProps, isDragActive} = useDropzone({ onDrop });
+  const onDrop = useCallback(async (acceptedFiles) => {
+    // console.log({acceptedFiles});
+    if ( acceptedFiles.length < 0 ) return;
+    if ( acceptedFiles[0].type != "image/jpeg" ) return;
+    const imageFile = acceptedFiles[0];
+    const imageDataUrl = await loadFile(imageFile);
+    console.log({imageDataUrl});
+  }, []);
 
   return (
-    <div {...getRootProps()}>
-      <input {...getInputProps()} />
-      {
-        isDragActive ?
-          <p>Drop the files here ...</p> :
-          <p>Drag "n" drop some files here, or click to select files</p>
-      }
-    </div>
+    <Dropzone onDrop={onDrop}>
+      {({getRootProps, getInputProps}) => (
+        <div {...getRootProps()}>
+          <input {...getInputProps()} />
+          <p>Drag 'n' drop some files here, or click to select files</p>
+        </div>
+      )}
+    </Dropzone>
   )
 }
 
