62fc1a64b5569c4f812cc587bf602b475a29ff52
kleamp1e <kleamp1e@gmail.com>
2022-02-19 18:03:12 +0900

    ファイルをドラッグ＆ドロップする処理を追加

--- frontend/app/src/App.js
+++ frontend/app/src/App.js
@@ -1,9 +1,33 @@
+import { useCallback } from "react";
+import { useDropzone } from "react-dropzone";
+
 import "./App.css";
+
+function MyDropzone() {
+  const onDrop = useCallback((acceptedFiles) => {
+    // Do something with the files
+    console.log({acceptedFiles});
+  }, [])
+  const {getRootProps, getInputProps, isDragActive} = useDropzone({ onDrop });
+
+  return (
+    <div {...getRootProps()}>
+      <input {...getInputProps()} />
+      {
+        isDragActive ?
+          <p>Drop the files here ...</p> :
+          <p>Drag "n" drop some files here, or click to select files</p>
+      }
+    </div>
+  )
+}
 
 export default function App() {
   return (
     <div className="App">
-      App
+      <h1>Pornstar Recognizer</h1>
+      <h1>Input</h1>
+      <MyDropzone />
     </div>
   );
 }
